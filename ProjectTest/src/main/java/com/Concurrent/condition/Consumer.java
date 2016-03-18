2f9398a6a1f6c5446156b272cf8bfe6492c08581
LUCKY <LUCKY@ZHOU>
2016-03-18 19:13:53 +0800

    delete error file

--- ProjectTest/src/main/java/com/Concurrent/condition/Consumer.java
+++ ProjectTest/src/main/java/com/Concurrent/condition/Consumer.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 /**
  * This class reads line from the buffer and process it
- *
+ * 
  */
 public class Consumer implements Runnable {
 
@@ -12,38 +12,39 @@ public class Consumer implements Runnable {
 	 * The buffer
 	 */
 	private Buffer buffer;
-	
+
 	/**
 	 * Constructor of the class. Initialize the buffer
+	 * 
 	 * @param buffer
 	 */
-	public Consumer (Buffer buffer) {
-		this.buffer=buffer;
+	public Consumer(Buffer buffer) {
+		this.buffer = buffer;
 	}
-	
+
 	/**
-	 * Core method of the consumer. While there are pending lines in the
-	 * buffer, try to read one.
+	 * Core method of the consumer. While there are pending lines in the buffer,
+	 * try to read one.
 	 */
-	@Override
 	public void run() {
 		while (buffer.hasPendingLines()) {
-			String line=buffer.get();
+			String line = buffer.get();
 			processLine(line);
 		}
 	}
 
 	/**
 	 * Method that simulates the processing of a line. Waits 10 milliseconds
+	 * 
 	 * @param line
 	 */
 	private void processLine(String line) {
 		try {
-			Random random=new Random();
+			Random random = new Random();
 			Thread.sleep(random.nextInt(100));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
 
 }
